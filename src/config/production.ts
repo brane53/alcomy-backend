0cf6927c3a1a59a1907b935a97459c2c1e25b251
Michael Kurrels <mjkurrels@gmail.com>
2017-11-22 17:21:48 -0800

    gets db info from env for prod

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,14 +2,14 @@ export const CONFIG = {
   port: process.env.PORT,
   logger: false,
   database: {
-    user: "brane",                    // user for the database
-    password: process.env.ALCOMY_DB_SECRET,           // password to access the database
-    database: "alcomy",               // name of the database
-    host: "https://www.alcomy.com",   // Server hosting the postgres database
-    port: 8080,                       // port the database is running on
-    max: 10,                          // max number of clients in the pool
-    min: 1,                           // min number of clients in the pool
-    idle: 30000                       // how long a client is allowed to remain idle before being closed (in milliseconds)
+    user: process.env.DB_USER,            // user for the database
+    password: process.env.DB_PASSWORD,    // password to access the database
+    database: process.env.DB_NAME,        // name of the database
+    host: process.env.DB_HOST,            // Server hosting the postgres database
+    port: process.env.DB_PORT,            // port the database is running on
+    max: 10,                              // max number of clients in the pool
+    min: 1,                               // min number of clients in the pool
+    idle: 30000                           // how long a client is allowed to remain idle before being closed (in milliseconds)
   },
   authConfig: {
     jwtSecret: process.env.ALCOMY_JWT_SECRET,
